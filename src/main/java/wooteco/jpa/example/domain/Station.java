593004504f30aa95bfc85a06566d0b248c2d24ec
hwookim <gusdn9754@gmail.com>
2020-07-02 16:10:18 +0900

    feat: Entity에 따른 table 자동 생성
    sql 로그를 위한 프로퍼티 추가

---
+++ src/main/java/wooteco/jpa/example/domain/Station.java
@@ -0,0 +1,27 @@
+package wooteco.jpa.example.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+//@Table : Column 같은 역할
+@Getter
+@Setter
+public class Station {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    // 꼭 필요하다. private로 선언하면 후에 어떤 문제가 생길까?
+    protected Station() {
+    }
+}
