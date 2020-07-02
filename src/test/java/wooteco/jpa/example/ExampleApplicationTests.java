0bb40172e8472941792d4abbe1d67d1ccff5b431
hwookim <gusdn9754@gmail.com>
2020-07-02 15:31:46 +0900

    Initial commit

---
+++ src/test/java/wooteco/jpa/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package wooteco.jpa.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
